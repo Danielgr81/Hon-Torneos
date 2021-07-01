dab54028c87bed01139b0a0be83e395bba42ed01
Daniel Garcia <86449690+Danielgr81@users.noreply.github.com>
2021-07-01 08:00:07 +0100

    Add files via upload

--- Web-Daniel/js/js.js
+++ Web-Daniel/js/js.js
@@ -1,3 +1,4 @@
+
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".btn-abrir");
 const closeMenuBtn = document.querySelector(".btn-cerrar");
@@ -8,3 +9,16 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+
+/*carusel*/
+
+$('.post-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.next'),
+
+});
